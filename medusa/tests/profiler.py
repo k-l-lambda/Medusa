c53900f1f0a3b29143f4f38e50977a5b1f3e648f
k.l.lambda <k.l.lambda@gmail.com>
2024-06-24 11:40:22 +0800

    profiler.py: added original inference mode.

--- medusa/tests/profiler.py
+++ medusa/tests/profiler.py
@@ -5,6 +5,7 @@ from fastchat.model.model_adapter import get_conversation_template
 import numpy as np
 
 from medusa.model.medusa_model import MedusaModel
+from medusa.model.kv_cache import initialize_past_key_values
 
 
 
@@ -29,27 +30,54 @@ def main (args):
 	output = ''
 	accepts = []
 
-	gen = model.medusa_generate(
-		input_ids,
-		temperature=args.temperature,
-		max_steps=args.max_steps,
-	)
+	if args.original_infer:
+		input_len = input_ids.shape[1]
 
-	for batch in gen:
-		output = batch['text']
-		new_token = batch['new_token']
-		accept_length = batch['accept_length']
-		accepts.append(accept_length)
+		while True:
+			(
+				past_key_values,
+				past_key_values_data,
+				current_length_data,
+			) = initialize_past_key_values(model.base_model)
 
-		print(f'{accept_length}/{new_token},	mem:{torch.cuda.memory_allocated(0):,}')
+			accepts.append(0)
+
+			medusa_logits, outputs, logits = model(
+				input_ids, past_key_values=past_key_values, output_orig=True, medusa_forward=True
+			)
+			new_id = torch.argmax(logits[:, -1])
+			new_id_value = new_id.item()
+			print(f'{new_id_value=},	{len(accepts)}	mem:{torch.cuda.memory_allocated(0):,}')
+
+			if new_id_value == model.tokenizer.eos_token_id:
+				output = model.tokenizer.decode(input_ids[0, input_len:], skip_special_tokens=True, spaces_between_special_tokens=False, clean_up_tokenization_spaces=True)
+				break
+
+			input_ids = torch.cat([input_ids, new_id[None, None]], dim=-1)
+	else:
+		gen = model.medusa_generate(
+			input_ids,
+			temperature=args.temperature,
+			max_steps=args.max_steps,
+		)
+
+		for batch in gen:
+			output = batch['text']
+			new_token = batch['new_token']
+			accept_length = batch['accept_length']
+			accepts.append(accept_length)
+
+			print(f'{accept_length}/{new_token},	mem:{torch.cuda.memory_allocated(0):,}')
 
 	print(f'{output=}')
 	print(f'{np.mean(accepts)=}')
 
 
 if __name__ == "__main__":
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--model", type=str, required=True, help="Model name or path.")
+	parser.add_argument("--original-infer", "-ori", action="store_true", help="Use original inference mode.")
 	parser.add_argument(
 		"--load-in-8bit", action="store_true", help="Use 8-bit quantization"
 	)
